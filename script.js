7eff1194a4d81277b562a13f26db649843dee699
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-01 00:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const evaluatedText = document.getElementById("evaluatedText");
+const wordCount = document.getElementById("wordCount");
+
+evaluatedText.addEventListener("input", function() {
+    let text = evaluatedText.value;
+    text = text.trim();
+    const count = text.split(/\s+/).length;
+    wordCount.textContent = count;
+});
